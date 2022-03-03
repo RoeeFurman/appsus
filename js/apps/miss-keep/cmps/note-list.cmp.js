3a7d1123ad11b6a68a00dcf2505f513f1fad0254
Michaela Bereza <michaelabereza@icloud.com>
2022-03-03 15:27:17 +0200

    making eventBus to sent note via mail

--- js/apps/miss-keep/cmps/note-list.cmp.js
+++ js/apps/miss-keep/cmps/note-list.cmp.js
@@ -6,7 +6,7 @@ export default {
     	<section class="notes-list">
             <ul class="notes-area">
                 <li class="note-card" v-for="note in notes" :class="[note.style.backgroundColor]">
-                    <note-preview :note="note" @cloneNote="cloneNote" @noteRemoved="removeNote" @updateColor="updateColor"></note-preview>
+                    <note-preview :note="note" @mailNote="mailNote" @cloneNote="cloneNote" @noteRemoved="removeNote" @updateColor="updateColor"></note-preview>
                 </li>
             </ul>
         </section>
@@ -25,6 +25,9 @@ export default {
     },
     cloneNote(noteId) {
       this.$emit("cloneNote", noteId);
+    },
+    mailNote(noteId) {
+      this.$emit("mailNote", noteId);
     },
   },
 };
