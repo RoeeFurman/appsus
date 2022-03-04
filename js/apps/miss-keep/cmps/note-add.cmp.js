2f11003022a9d6f6a1709175a14b0f09b040fc62
Michaela Bereza <michaelabereza@icloud.com>
2022-03-04 18:47:11 +0200

    support to add more types of notes

--- js/apps/miss-keep/cmps/note-add.cmp.js
+++ js/apps/miss-keep/cmps/note-add.cmp.js
@@ -2,16 +2,16 @@ export default {
   template: `
             <section class="note-add">
                 <form @submit="addNote">
-                    <input type="text" class="title" placeholder="Note title" v-model="newNote.titleTxt">
-                    <input type="text" class="content" placeholder="Whats on your mind?" v-model="newNote.info.txt">
+                    <input type="text" class="title" placeholder="Note title" v-model="titleTxt">
+                    <input type="text" class="content" placeholder="Whats on your mind?" v-model="info">
                     <div class="add-actions">
-                    <button @click="addImgNote">
+                    <button @click="setType('note-img')">
                         <img src="img-notes/bx-image.svg" alt="image">
                     </button>
-                    <button @click="addYoutubeNote">
+                    <button @click="setType('note-video')">
                         <img src="img-notes/bxl-youtube.svg" alt="youtube">
                     </button>
-                    <button @click="addListNote">
+                    <button @click="setType('note-todos')">
                         <img src="img-notes/bx-list-ul.svg" alt="list">
                     </button>
                     </div>
@@ -24,21 +24,32 @@ export default {
         `,
   data() {
     return {
-      newNote: {
-        type: "note-txt",
-        titleTxt: "",
-        info: { txt: "" },
-      },
+      type: "note-txt",
+      titleTxt: "",
+      info: "",
     };
   },
   created() {},
   methods: {
     addNote() {
-      if (!this.newNote.info.txt && !this.newNote.titleTxt) return;
-      this.$emit("addNote", this.newNote);
+      if (!this.info && !this.titleTxt) return;
+      const note = { type: this.type, titleTxt: this.titleTxt };
+      if (this.type === "note-txt") {
+        note.info = { txt: this.info };
+      }
+      if (this.type === "note-img") {
+        note.info = { url: this.info };
+      }
+      if (this.type === "note-video") {
+        note.info = { url: this.info };
+      }
+      if (this.type === "note-todos") {
+        note.info = { todos: [{ txt: this.info }] };
+      }
+      this.$emit("addNote", note);
     },
-    addImgNote() {
-      if (!this.newNote.info.url && !this.newNote.titleTxt) return;
+    setType(type) {
+      this.type = type;
     },
   },
 };
