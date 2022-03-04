35459e22bf119bbc14e23000b0a95974d948da9a
Michaela Bereza <michaelabereza@icloud.com>
2022-03-04 13:17:26 +0200

    fix red green from books

--- js/apps/miss-books/js/views/book-details.js
+++ js/apps/miss-books/js/views/book-details.js
@@ -3,7 +3,7 @@ import reviewAdd from "../cmps/review-add.js";
 import { bookService } from "../services/book-service.js";
 
 export default {
-    template: `
+  template: `
                 <section v-if="book" class="book-details">
                     <router-link class="back-link" to="/book/">Back to Books</router-link><br>
                     <img :src="bookImgUrl" class="book-details-img">
@@ -24,69 +24,71 @@ export default {
 
                 </section>
                 `,
-    components: {
-        longText,
-        reviewAdd
+  components: {
+    longText,
+    reviewAdd,
+  },
+  data() {
+    return {
+      book: null,
+      bookId: null,
+    };
+  },
+  created() {
+    const id = this.$route.params.bookId;
+    this.bookId = id;
+    console.log(this.$route.params.bookId);
+    bookService.get(id).then((book) => {
+      console.log(book.prevbookId, book.nextbookId);
+      this.book = book;
+    });
+  },
+  computed: {
+    authorsForDisplay() {
+      return this.book.authors.join(", ");
     },
-    data() {
-        return {
-            book: null,
-            bookId: null,
-        };
-    },
-    created() {
-        const id = this.$route.params.bookId;
-        this.bookId = id;
-        console.log(this.$route.params.bookId)
-        bookService.get(id)
-            .then(book => {
-                console.log(book.prevbookId, book.nextbookId);
-                this.book = book
-            });
-    },
-    computed: {
-        authorsForDisplay() {
-            return this.book.authors.join(', ')
-        },
-        displayPageCount() {
-            let val = '';
-            if (this.book.pageCount > 500) val = 'Long Reading'
-            else if (this.book.pageCount > 200) val = 'Decent Reading'
-            else val = 'Light Reading'
+    displayPageCount() {
+      let val = "";
+      if (this.book.pageCount > 500) val = "Long Reading";
+      else if (this.book.pageCount > 200) val = "Decent Reading";
+      else val = "Light Reading";
 
-            return this.book.pageCount + ' pages, ' + val
-        },
-        displayPublishedDate() {
-            let date = new Date().getFullYear()
-            let val = '';
-            let calcYears = date - this.book.publishedDate;
-            if (calcYears > 10) val = 'Veteran Book';
-            if (calcYears < 1) val = 'New!';
-
-            return this.book.publishedDate + ' ' + val
-        },
-        displaycategories() {
-            return this.book.categories.join(', ')
-        },
-        bookImgUrl() {
-            return this.book.thumbnail
-        },
-        displayPriceColor() {
-            if (this.book.listPrice.amount > 150) return 'red'
-            if (this.book.listPrice.amount < 20) return 'green'
-        }, formatCurrency() {
-            let val = this.book.listPrice.currencyCode;
-            return new Intl.NumberFormat(val, { style: 'currency', currency: val }).format(this.book.listPrice.amount);
-         }
+      return this.book.pageCount + " pages, " + val;
     },
-    watch: {
-        '$route.params.bookId': {
-            handler() {
-                this.bookId = this.$route.params.bookId;
-                bookService.get(this.bookId)
-                .then(book => this.book = book);
-            },
-            immediate: true,
-        }
-    }
-}
+    displayPublishedDate() {
+      let date = new Date().getFullYear();
+      let val = "";
+      let calcYears = date - this.book.publishedDate;
+      if (calcYears > 10) val = "Veteran Book";
+      if (calcYears < 1) val = "New!";
+
+      return this.book.publishedDate + " " + val;
+    },
+    displaycategories() {
+      return this.book.categories.join(", ");
+    },
+    bookImgUrl() {
+      return this.book.thumbnail;
+    },
+    // displayPriceColor() {
+    //   if (this.book.listPrice.amount > 150) return "expensive";
+    //   if (this.book.listPrice.amount < 20) return "cheap";
+    // },
+    formatCurrency() {
+      let val = this.book.listPrice.currencyCode;
+      return new Intl.NumberFormat(val, {
+        style: "currency",
+        currency: val,
+      }).format(this.book.listPrice.amount);
+    },
+  },
+  watch: {
+    "$route.params.bookId": {
+      handler() {
+        this.bookId = this.$route.params.bookId;
+        bookService.get(this.bookId).then((book) => (this.book = book));
+      },
+      immediate: true,
+    },
+  },
+};
