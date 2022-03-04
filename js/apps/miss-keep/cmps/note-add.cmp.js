acbb841204a7949714af49c8319ffa4f7b1bb6a5
Michaela Bereza <michaelabereza@icloud.com>
2022-03-04 17:40:53 +0200

    added more buttons for type of notes to be sdded

--- js/apps/miss-keep/cmps/note-add.cmp.js
+++ js/apps/miss-keep/cmps/note-add.cmp.js
@@ -4,6 +4,17 @@ export default {
                 <form @submit="addNote">
                     <input type="text" class="title" placeholder="Note title" v-model="newNote.titleTxt">
                     <input type="text" class="content" placeholder="Whats on your mind?" v-model="newNote.info.txt">
+                    <div class="add-actions">
+                    <button @click="addImgNote">
+                        <img src="img-notes/bx-image.svg" alt="image">
+                    </button>
+                    <button @click="addYoutubeNote">
+                        <img src="img-notes/bxl-youtube.svg" alt="youtube">
+                    </button>
+                    <button @click="addListNote">
+                        <img src="img-notes/bx-list-ul.svg" alt="list">
+                    </button>
+                    </div>
                     <button type="submit">
                         <img src="img-notes/bx-plus.svg" alt="plus">
                     </button>
@@ -25,6 +36,9 @@ export default {
     addNote() {
       if (!this.newNote.info.txt && !this.newNote.titleTxt) return;
       this.$emit("addNote", this.newNote);
+    },
+    addImgNote() {
+      if (!this.newNote.info.url && !this.newNote.titleTxt) return;
     },
   },
 };
--- js/apps/miss-keep/cmps/note-todos.cmp.js
+++ js/apps/miss-keep/cmps/note-todos.cmp.js
@@ -1,18 +1,33 @@
 export default {
   props: ["info"],
   template: `
-              <section class="note-todos">
+            <section class="note-todos">
+              <div v-if="openTodos.length > 0">
                 <ul>
-                  <li>
-                    <input type="checkbox">
-                    {{info.todos[0].txt}}
-                  </li>
-                  <li>
-                  <input type="checkbox">
-                    {{info.todos[1].txt}}
+                  <li v-for="todo in openTodos">
+                    <input type="checkbox" :checked="todo.isDone">
+                    {{todo.txt}}
                   </li>
                 </ul>
-              </section>
+              </div>
+              <div v-if="closeTodos.length > 0">
+                <ul>
+                  <li v-for="todo in closeTodos" class="done">
+                  <input type="checkbox" :checked="todo.isDone">
+                    {{todo.txt}}
+                  </li>
+                </ul>
+              </div>
+            </section>
           `,
   components: {},
+  computed: {
+    openTodos() {
+      return this.info.todos.filter((todo) => todo.isDone === false);
+    },
+
+    closeTodos() {
+      return this.info.todos.filter((todo) => todo.isDone === true);
+    },
+  },
 };
