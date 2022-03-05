5e1b20fe3b44a710ff75ac50c56b89dc1e08d4d4
Roee Furman <roeefurman1@gmail.com>
2022-03-05 20:50:28 +0200

    icon

--- js/cmps/app-header.js
+++ js/cmps/app-header.js
@@ -1,7 +1,7 @@
 export default {
     template:`
         <header class="app-header ">
-                 <router-link to="/" class="logo">Appsus</router-link> 
+                 <router-link to="/" class="logo">Appsus <img src="img-notes/icons8-horse-stable-60.png" class="logo-main-icon"/></router-link> 
         </header>
     
     `
